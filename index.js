13340938ea0184d888e77fcc3adc2b3e256fb731
Nommiiee <kunalkashyp3@gmail.com>
2022-09-04 16:30:26 +0530

    temp

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
-require('dotenv').config();
+require("dotenv").config();
 //const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
-const fs = require('node:fs');
-const path = require('node:path');
+const { token } = require("./config.json");
+const fs = require("node:fs");
+const path = require("node:path");
 
 // Require the necessary discord.js classes
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
 }
 
-client.login(token); 
+client.login(token);
 
-let random = Math.floor((Math.random() * 100) + 1);
-let reminder = random%20;
+let random = Math.floor(Math.random() * 100 + 1);
+let reminder = random % 20;
 console.log(random);
-if(random == 100 || random == 1)
-console.log('Legendary');
-else if(reminder == 19){
-console.log('rare');
-}
-else if(reminder !== 19)
-{
-	reminder = random%10;
-	if(reminder == 9)
-	console.log('common');
-}
-else
-console.log('eggs');
+if (random == 100 || random == 1) console.log("Legendary");
+else if (reminder == 19) {
+  console.log("rare");
+} else if (reminder !== 19) {
+  reminder = random % 10;
+  if (reminder == 9) console.log("common");
+} else console.log("eggs");
 console.log(token);
